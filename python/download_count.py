c68e401e27e441004dab3c041dc1834bded6c53a
Keeema-1 <92374693+Keeema-1@users.noreply.github.com>
2023-08-25 14:29:07 +0900

    update

--- python/download_count.py
+++ python/download_count.py
@@ -1,11 +1,15 @@
 import requests
 
+total = 0
 r = requests.get('https://api.github.com/repos/Keeema-1/MobTamer/releases')
 for item in r.json():
     # print("tag_name: ", item["tag_name"])
     if (len(item["assets"])>0) and ("download_count" in item["assets"][0]):
         # print(item)
         print("name: ", item["name"])
-        print("published_at: ", item["published_at"])
+        print("published_at: ", item["published_at"].split("T")[0])
         print("download count: ", item["assets"][0]["download_count"])
+        total += item["assets"][0]["download_count"]
         print("")
+
+print("total: ", total)
